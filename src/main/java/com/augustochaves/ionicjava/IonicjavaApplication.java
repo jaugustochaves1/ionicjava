ad09a53042601b13340d574c2720a0b966740675
José Augusto Chaves Rangel <jaugustochaves@gmail.com>
2019-01-30 23:19:19 -0200

    Iniciando o projeto

---
+++ src/main/java/com/augustochaves/ionicjava/IonicjavaApplication.java
@@ -0,0 +1,14 @@
+package com.augustochaves.ionicjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonicjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonicjavaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/augustochaves/ionicjava/IonicjavaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.augustochaves.ionicjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IonicjavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
